c161708adeea520a13d7b3e318eb65896dec3b86
JeffJerseyCow <jeffjerseycow@gmail.com>
2019-07-11 08:36:40 +0100

    improved tinkerpop image

--- tinkerpop/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-IP=$(ip -o -4 addr list eth0 | perl -n -e 'if (m{inet\s([\d\.]+)\/\d+\s}xms) { print $1 }')
-sed -i "s|^host:.*|host: $IP|" /opt/gremlin-server/conf/gremlin-server-neo4j.yaml
-/opt/gremlin-server/bin/gremlin-server.sh /opt/gremlin-server/conf/gremlin-server-neo4j.yaml
